c44bea004486f4ef868a33b2522d3f1da1e96f8d
priyaranjan <jena.priya13@gmail.com>
2024-05-10 14:37:05 +0530

    first commit

---
+++ src/displayName.jsx
@@ -0,0 +1,64 @@
+import { useState } from "react";
+
+
+export default function Display(){
+    const [formData, setFormData] = useState({
+        firstName: "",
+        lastName: ""
+    });
+
+    const [fullName, setFullName] = useState('')
+
+    const handleSubmit = (e) =>{
+        e.preventDefault();
+        setFullName(`${formData.firstName} ${formData.lastName}`)
+        setFormData({
+            firstName: "",
+            lastName: ""
+        })
+    }
+
+    return(
+        <>
+        <h1>Full Name Display</h1>
+        <div className="form">
+            <form onSubmit={handleSubmit}>
+                <label>First Name: </label>
+                <input required 
+                    type="text" 
+                    name="firstName"
+                    value={formData.firstName}
+                    onChange={(e)=>{
+                        setFormData({
+                            ...formData,
+                            firstName: e.target.value
+                        })
+                    }}
+                    />
+                <br/>
+                <br />
+                <label>Last Name: </label>
+                <input required
+                    type="text" 
+                    name="lastName"
+                    value={formData.lastName}
+                    onChange={(e)=>{
+                        setFormData({
+                            ...formData,
+                            lastName: e.target.value
+                        })
+                    }} />
+                <br/>
+                <br />
+                <button type="submit">Submit</button>
+            </form>
+        </div>
+        <div>
+            <p>Full Name: {fullName}</p>
+
+        </div>
+       
+        </>
+
+    )
+}
